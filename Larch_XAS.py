3048432cf455e29145ca44f67c417fda013548ef
Cheng-Chu Chung <70043815+JamesChung821@users.noreply.github.com>
2022-09-03 16:51:36 -0400

    plot config

--- Larch_XAS.py
+++ Larch_XAS.py
@@ -14,37 +14,50 @@ from xraydb import guess_edge, xray_edge
 from larch.xafs import pre_edge, find_e0
 from larch.io import read_ascii, write_ascii, merge_groups, read_athena, create_athena, write_group
 import athena_project
-import palettable.colorbrewer.diverging as pld
+import palettable as pltt
 from collections import defaultdict
 from pprint import pprint
+import configparser
 
 # Constant
-FILE_TYPE = '.dat'  # ".prj" for merging fluorescence scans; '', '.dat' for merging transmission scans; ".txt" for plotting scans
-INPUT_PATH = r'D:\Research data\SSID\202208'
+"""
+FILE_TYPE instructions:
+'.prj' for merging fluorescence scans
+'' or '.dat' for merging transmission scans
+'.txt' for plotting scans
+"""
+FILE_TYPE = '.dat'
+INPUT_PATH = r'D:\Research data\SSID\202209\20220330 ISS NbOx'
 
 # Merged Constant
-SKIP_SCANS = ['MnO2_45_16C_Charge_Mn_001', 'EPC_02_Mn_003']     # [] if scans are good or just add scans you want to exclude
+SKIP_SCANS = ['MnO2_45_16C_Charge_Mn_001']     # [] if scans are good or just add scans you want to exclude
 IF_NOR = False   # Do normalization for fluorescence scans
 ADD_DEV = False     # Add plus and minus standard deviation lines for fluorescence scans
 SHOW_DATA_INFORMATION = False   # List athena parameters, such as atomic symbol, edge, label, etc.
 
-# Plot Constant
+# Plot Constant for .txt
 """
 You could set FILE_INDEX = 0, SAMPLE_LIST = [], STANDARD_LIST = [], 
 SAMPLE_LABEL = [], ENERGY_RANGE = () as a default for your first try.
 """
-FILE_INDEX = 0  # Which file in the file list you want to plot
-SAMPLE_LIST = [10, 9, 11, 8, 7]     # [] for default or [1, 7, 5, 3] for a index list you want to plot
-STANDARD_LIST = [7]      # [] if no standards in the SAMPLE_LIST or [5, 3] in the SAMPLE_LIST become dash lines
-SAMPLE_LABEL = ['Pristine', '900C00M', '900C7p5M', '900C60M', 'Pure Nb']  # [] for default or add a specific name list
-FIGURE_SIZE = (6.4, 4.8)  # Cheng-hung uses (6, 7.5), but the default is (6.4, 4.8)
-PALETTE = pld.Spectral_4_r  # _r if you want to reverse the color sequence
+CONFIG_FILE = r"D:\Research data\SSID\202205\20220509 20210221 BMM\b28_Sc_pure_config.ini"
+config = configparser.ConfigParser()
+config.read(CONFIG_FILE, encoding="utf8")
+
+FILE_INDEX = config.getint('samples', 'file_index')  # Which file in the file list you want to plot
+SAMPLE_LIST = eval(config['samples']['sample_list'])  # [] for default or [1, 7, 5, 3] for a index list you want to plot
+STANDARD_LIST = eval(config['samples']['standard_list'])  # [] if none or [5, 3] in the SAMPLE_LIST become dash lines
+SAMPLE_LABEL = eval(config['legends']['sample_label'])  # [] for default or add a specific name list
+FIGURE_SIZE = eval(config['format']['figure_size'])  # Cheng-Hung uses (6, 7.5), but the default is (6.4, 4.8)
+PALETTE = eval(config['format']['palette'])     # pld.Spectral_4_r  # _r if you want to reverse the color sequence
 CMAP = PALETTE.mpl_colormap     # .mpl_colormap attribute is a continuous, interpolated map
-OFFSET = 0.15  # Value you want to add to an y offset for each curve.
-ENERGY_RANGE = (18900, 19150)   # () for default, (18900, 19150) for Nb, (4425, 4625) for Sc
-ENERGY_INTERVAL = 50   # This parameter works only when you set a ENERGY_RANGE
-IF_SAVE = True
-OUTPUT_FILENAME = 'b31-Nb-time-dependent-rect'
+COLOR_INCREMENT = eval(config['format']['color_increment'])
+OFFSET = eval(config['format']['offset'])   # Value you want to add to an y offset for each curve.
+ENERGY_RANGE = eval(config['format']['energy_range'])   # () for default, (18900, 19150) for Nb, (4425, 4625) for Sc
+ENERGY_INTERVAL = eval(config['format']['energy_interval'])   # This parameter works only when you set a ENERGY_RANGE
+IF_SAVE = eval(config['format']['if_save'])
+OUTPUT_FILENAME = config['format']['output_filename']
+NUM_COLUMN = 1
 
 
 def main():
@@ -64,7 +77,7 @@ def main():
         print('=================================================================================')
 
         # Call Path again to grab the created athena project
-        files = Path(INPUT_PATH).glob(f'*{FILE_TYPE}')
+        files = Path(INPUT_PATH).glob(f'*.prj')
         calibrate_energy(files)
 
     elif FILE_TYPE == '.txt':
@@ -82,6 +95,7 @@ def main():
             if '.prj' in file_prj.name and 'Created' not in file_prj.name:
                 group = read_ascii(f'{file_prj}')       # <------------------------- take care, not read_athena
                 group.filename = group.filename[:-4]    # <------------------------- take care, rename group filename
+
                 if SHOW_DATA_INFORMATION:
                     print("\n==============================")
                     print(f'Scan attributes in {group.filename}')
@@ -94,7 +108,7 @@ def main():
                 new_merge_project.add_group(group, sample_name)
 
         new_merge_project.save(f'{Path(INPUT_PATH)}/Created_transmission_group.prj')
-        print('\n=================================================================================')
+        print('=================================================================================')
         print(f'Save merge project into ---> Created_transmission_group.prj')
         print('=================================================================================')
 
@@ -117,7 +131,7 @@ def plot_xas(files):
         f_list.append(file)
         print(index, file)
 
-    print("==============================")
+    print("\n==============================")
     print(f'Data column in file number {FILE_INDEX}')
     print("------------------------------")
     file = read_ascii(f_list[FILE_INDEX])
@@ -129,25 +143,26 @@ def plot_xas(files):
 
     # Do the plotting
     f1, ax1 = plt.subplots(1, 1, figsize=FIGURE_SIZE)
-    print("==============================")
+    print("\n==============================")
     print('Index   Filename')
     print("------------------------------")
     increment = 0   # Increment for offset
     if len(SAMPLE_LIST) == 0:
-        color_idx = np.linspace(0, 1, len(file_keys)-7)   # All plots have their own color
+        color_idx = np.linspace(0, 1, len(file_keys)-7+COLOR_INCREMENT)   # All plots have their own color
         for i in range(len(file_keys)-7):   # Start from the first sample name because file key 0-6 are data information
             sample_index = i + 7
             sample_name = file_keys[sample_index]
-            if len(SAMPLE_LABEL) > i:
-                sample_label = SAMPLE_LABEL[i]
-            else:
-                sample_label = sample_name
+            sample_label = sample_name
+            # if len(SAMPLE_LABEL) > i:
+            #     sample_label = SAMPLE_LABEL[i]
+            # else:
+            #     sample_label = sample_name
             mu = getattr(file, file_keys[sample_index])
-            ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment]), label=sample_label)
+            ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment+COLOR_INCREMENT]), label=sample_label)
             increment += 1
             print('{:>3}     {}'.format(sample_index, sample_name))
     else:
-        color_idx = np.linspace(0, 1, len(SAMPLE_LIST))     # Only the plots you want have their own color
+        color_idx = np.linspace(0, 1, len(SAMPLE_LIST)+COLOR_INCREMENT)     # Only the plots you want have their own color
         for sample_index in SAMPLE_LIST:
             sample_name = file_keys[sample_index]
             if len(SAMPLE_LABEL) > SAMPLE_LIST.index(sample_index):
@@ -156,9 +171,9 @@ def plot_xas(files):
                 sample_label = sample_name
             mu = getattr(file, file_keys[sample_index])
             if sample_index in STANDARD_LIST:
-                ax1.plot(energy, mu + OFFSET * increment, '--', color=CMAP(color_idx[increment]), label=sample_label)
+                ax1.plot(energy, mu + OFFSET * increment, '--', color=CMAP(color_idx[increment+COLOR_INCREMENT]), label=sample_label)
             else:
-                ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment]), label=sample_label)
+                ax1.plot(energy, mu + OFFSET * increment, color=CMAP(color_idx[increment+COLOR_INCREMENT]), label=sample_label)
             increment += 1
             print('{:>3}     {}'.format(sample_index, sample_name))
 
@@ -176,10 +191,11 @@ def plot_xas(files):
     ax1.set_xlabel(x_label, fontsize=18)
     ax1.set_ylabel(y_label, fontsize=18)
     plt.rcParams["axes.linewidth"] = 5
-    plt.legend(loc='lower right', framealpha=1, frameon=False, fontsize=14)
+    plt.legend(loc='lower right', framealpha=1, frameon=False, fontsize=14, ncol=NUM_COLUMN)
     plt.tight_layout()
     if IF_SAVE:
-        plt.savefig("{}/{}.png".format(Path(INPUT_PATH), OUTPUT_FILENAME), dpi=300, transparent=False)
+        output_filename = check_filename_repetition(OUTPUT_FILENAME)
+        plt.savefig("{}/{}.png".format(Path(INPUT_PATH), output_filename), dpi=300, transparent=False)
     plt.show()
 
 
@@ -226,7 +242,7 @@ def merge_scan(file_prj, new_merge_project):
     # Do the merge and plot
     merges = merge_groups(scans_grouplist)
     if IF_NOR:  # Do normalization
-        pre_edge(merges.energy, merges.mu, group=merges)
+        pre_edge(merges.energy, merges.mu, group=merges)    # <---------- Automatically define white region
         plt.plot(merges.energy, merges.flat, label=f'{first_scan_information.label[:-4]}_merged')
         if ADD_DEV:
             plt.plot(merges.energy, merges.flat + merges.mu_std * merges.flat / merges.mu, '-',
@@ -292,7 +308,7 @@ def read_transmission(files):
         scan = scan.resolve()  # Make the path absolute, resolving any symlinks
         scanname = scan.name
 
-        if scanname[-3:].isnumeric() or scanname[-3:] == 'dat':   # <------------------------------------------- file type .001, .002, .003, etc.
+        if scanname[-3:].isnumeric() or scanname[-3:] == 'dat':   # <---- file type .001, .002, .003, or 0001.dat, etc.
             print(index, scanname)
             scan = read_ascii(scan)
 
@@ -308,10 +324,11 @@ def read_transmission(files):
                 show_data_information(scan)
                 print('')
 
-            # Append energy, mu
+            # Append energy, mu!!!
             if scanname[-3:] == 'dat':
                 space_index = scanname.find(' ', -11)
-                sample_name = scanname[:space_index].replace('-', '_').replace('(', '').replace(')', '').replace(' ', '_')
+                sample_name = scanname[:space_index].replace('-', '_').replace('(', '').replace(')', '').replace(' ',
+                                                                                                                 '_')
 
                 if f'{sample_name}_energy_mu' not in scan_dictionary:
                     scan_dictionary[f'{sample_name}_energy_mu'] = []
@@ -323,7 +340,7 @@ def read_transmission(files):
                     scan_dictionary[f'{sample_name}_energy_mu'].append(np.log(scan.i0 / scan.it))   # <--- Transmission
 
             else:
-                sample_name = scanname[:-4]
+                sample_name = scanname[:-4].replace('-', '_').replace('(', '').replace(')', '').replace(' ', '_')
                 if f'{sample_name}_energy_mu' not in scan_dictionary:
                     scan_dictionary[f'{sample_name}_energy_mu'] = []
                     scan_dictionary[f'{sample_name}_energy_mu'].append(scan.energy)                   # <--- Energy
@@ -335,7 +352,7 @@ def read_transmission(files):
 
     # Append merged data, so each item will contain energy, reference, scan1, scan2, scan3, etc... and a merged scan.
     print("\n==============================")
-    print('Scan plot')
+    print('Scan plot and you could copy the scan name below you want to exclude into the SKIP_SCANS')
     print("------------------------------")
 
     for sample_data in scan_dictionary:
@@ -365,6 +382,7 @@ def read_transmission(files):
                 if f'{sample_name}_00{mu_index - 1 + skip_times}' in SKIP_SCANS:
                     skip_times += 1
                 label = f'{sample_name}_00{mu_index - 1 + skip_times}'
+
             if mu_index > 1:
                 print(label)
                 plt.plot(energy, mu, label=label)
@@ -382,7 +400,7 @@ def read_transmission(files):
         plt.tight_layout()
         if IF_SAVE:
             plt.savefig("{}/{}.png".format(Path(INPUT_PATH), f'{sample_name}'), dpi=300, transparent=False)
-            print('=================================================================================')
+            print('\n=================================================================================')
             print(f'Save figures into ---> {sample_name}.png')
             print('=================================================================================')
             print('')
@@ -411,7 +429,7 @@ def calibrate_energy(files):
                 data.xrayedge = xray_edge(data.atsym, data.edge)[0]
                 print(name, data)
                 print('e0:', e0)
-                print(f'{data.atsym} energy edge', data.xrayedge)
+                print(f'{data.atsym} energy edge:', data.xrayedge)
                 if 'foil' in data.label or 'reference' in data.label:    # <------------------------- reference keyword
                     data.energyshift = data.xrayedge - data.e0
                     print('Energy shift:', data.energyshift)
@@ -460,10 +478,12 @@ def calibrate_energy(files):
             if data.label[:-9] in reference_name and data.label[:-6] in reference_name:
                 print(index, data.label)
                 energy_shift = reference_energy_shift_dictionary[reference_name]
-                print('Energy before:', data.energy[0])
+                # print('Energy before:', data.energy[0])
+                print('Energy E0 before:', data.e0)
                 print('Energy shift:', energy_shift)
                 data.energy = data.energy + energy_shift    # <------------------------------------- Energy calibration
-                print('Energy after:', data.energy[0])
+                # print('Energy after:', data.energy[0])
+                print('Energy E0 after:', find_e0(data.energy, mu=data.mu, group=data))
                 reference_checklist.append(reference_name)
                 print('')
 
@@ -484,6 +504,26 @@ def show_data_information(group):
     """
     for scan_attribute in dir(group):
         print(scan_attribute, type(getattr(group, scan_attribute)))
+
+
+def check_filename_repetition(output_filename):
+    """
+    :param output_filename: string, output filename
+    :return: string, new output filename
+    """
+    print("\n==============================")
+    print('Check filename repetition')
+    print("------------------------------")
+    files = Path(INPUT_PATH).glob(f'*.png')
+    png_list = []
+    for index, file in enumerate(files):
+        png_list.append(file.name[:-4])
+
+    print(output_filename)
+    while output_filename in png_list:
+        output_filename = output_filename + '_1'
+        print(output_filename)
+    return output_filename
 
 
 if __name__ == '__main__':
